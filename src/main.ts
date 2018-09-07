4ea80600f4d8eecf790e9593c55b945c450ad3c8
KlarY <klar0928@gmail.com>
2018-09-07 15:56:57 +0800

    change https

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as express from 'express';
+import * as http from 'http';
+import * as https from 'https';
 
 async function bootstrap() {
   const httpsOptions = {
     key: fs.readFileSync(path.join(__dirname, '../ssl_cert/214979365060370.key')),
     cert: fs.readFileSync(path.join(__dirname, '../ssl_cert/214979365060370.pem')),
   };
-  const app = await NestFactory.create(AppModule, httpsOptions);
-  await app.listen(3000);
+  const server = express();
+  const app = await NestFactory.create(AppModule, server);
+  await app.init();
+
+  http.createServer(server).listen(3000);
+  https.createServer(httpsOptions, server).listen(3001);
+  // await app.listen(3000);
 }
 bootstrap();
